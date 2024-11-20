2986ca35b36c1d12c188261c73d3fa518f7076a7
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-21 02:09:56 +0300

    Proje dosyası ekle.

---
+++ 221120241056_Mehmet Kerim_Gündemir/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221120241056_Mehmet_Kerim_Gündemir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = int.Parse(Console.ReadLine());
+
+
+            int toplam = 0;
+
+            for (int i = 1; i <= sayi1; i++)
+            {
+                toplam += i;
+
+            }
+            Console.WriteLine(toplam);
+
+            Console.Read();
+        }
+    }
+}
